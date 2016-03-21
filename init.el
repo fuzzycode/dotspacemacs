02d78ad2766f2b58443dd7dea0518d050a94e6ab
Björn Larsson <b.e.larsson@gmail.com>
2016-03-21 22:04:25 +0100

    Enable default packages

--- init.el
+++ init.el
@@ -23,18 +23,21 @@ values."
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     ;; auto-completion
+      auto-completion
      ;; better-defaults
      emacs-lisp
-     ;; git
-     ;; markdown
-     ;; org
+
+     (git :variables
+          git-magit-status-fullscreen t)
+
+     markdown
+     org
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
-     ;; spell-checking
-     ;; syntax-checking
-     ;; version-control
+     spell-checking
+     syntax-checking
+     version-control
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
