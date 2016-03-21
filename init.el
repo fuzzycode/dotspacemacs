2fab891a99061d4c5bf620e24dba2fb80bbdc336
Björn Larsson <b.e.larsson@gmail.com>
2016-03-21 22:08:24 +0100

    Enable shell layer

--- init.el
+++ init.el
@@ -32,9 +32,9 @@ values."
 
      markdown
      org
-     ;; (shell :variables
-     ;;        shell-default-height 30
-     ;;        shell-default-position 'bottom)
+     (shell :variables
+             shell-default-height 30
+             shell-default-position 'bottom)
      spell-checking
      syntax-checking
      version-control
