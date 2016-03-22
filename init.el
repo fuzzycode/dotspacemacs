22fa10845cef7b14cc65627c110d9e99741aef14
Björn Larsson <b.e.larsson@gmail.com>
2016-03-22 21:08:34 +0100

    Added bl-dired layer
    - Adds peep-dired package and configuration

--- init.el
+++ init.el
@@ -38,6 +38,9 @@ values."
      spell-checking
      syntax-checking
      version-control
+
+     ;; My private packages
+     bl-dired
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
