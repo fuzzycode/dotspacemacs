59383ee2928416700e81aa480afea48f3977162a
Björn Larsson <b.e.larsson@gmail.com>
2016-04-07 22:19:43 +0200

    Added navigation key-binding
     - Added C-- For pop-global-mark

---
+++ layers/bl-edit/keybindings.el
@@ -0,0 +1,11 @@
+;;; keybindings.el --- bl-edit Layer key-bindings File
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Björn Larsson <develop@bjornlarsson.net>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+(global-set-key (kbd "C--") 'pop-global-mark)
