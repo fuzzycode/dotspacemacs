069f6d54cc2a292f2d10c2a593e0676efb519708
Björn Larsson <b.e.larsson@gmail.com>
2016-04-24 21:43:38 +0200

    Added "bb" for helm-mini key-chord.

--- layers/bl-key-chords/packages.el
+++ layers/bl-key-chords/packages.el
@@ -68,6 +68,7 @@ Each entry is either:
       (key-chord-define-global "jj" 'avy-goto-word-1)
       (key-chord-define-global "uu" 'undo-tree-visualize)
       (key-chord-define-global "xx" 'helm-M-x)
+      (key-chord-define-global "bb" 'helm-mini)
       (key-chord-mode t)
       )
     )
