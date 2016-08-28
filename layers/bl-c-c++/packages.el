7129f796a9170a8f3cf8db3a2192acc335303f2e
Björn Larsson <b.e.larsson@gmail.com>
2016-08-28 21:38:42 +0200

    Setup irony for completion with company

--- layers/bl-c-c++/packages.el
+++ layers/bl-c-c++/packages.el
@@ -30,7 +30,7 @@
 ;;; Code:
 
 (defconst bl-c-c++-packages
-  '(cmake-ide rtags company irony company-irony company-irony-c-headers flycheck-irony)
+  '(company irony company-irony company-irony-c-headers irony-eldoc)
   "The list of Lisp packages required by the bl-c-c++ layer.
 
 Each entry is either:
@@ -59,126 +59,29 @@ Each entry is either:
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
 
 (defun bl-c-c++/post-init-company ()
-  (setq company-idle-delay 0.5))
+  (setq company-idle-delay 0.7))
 
 (defun bl-c-c++/init-irony ()
   (use-package irony
-    :defer t
     :commands (irony-mode)
     :init (progn
               (add-hook 'c++-mode-hook 'irony-mode)
               (add-hook 'c-mode-hook 'irony-mode)
               (add-hook 'objc-mode-hook 'irony-mode)
+
+              (defun irony/irony-mode-hook ()
+                (define-key irony-mode-map [remap completion-at-point] 'irony-completion-at-point-async)
+                (define-key irony-mode-map [remap complete-symbol] 'irony-completion-at-point-async))
+
+              (add-hook 'irony-mode-hook 'irony/irony-mode-hook)
+              (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
               (spacemacs|diminish irony-mode " Ⓘ" " I"))
     ))
 
-(defun bl-c-c++/init-flycheck-irony ()
-  (use-package flycheck-irony
-    :defer t
-    :init (eval-after-load 'flycheck '(add-to-list 'flycheck-checkers 'irony))))
-
-(defun bl-c-c++/init-rtags ()
-  (use-package rtags
-    :ensure company
-    :config
+(defun bl-c-c++/init-company-irony ()
+  (use-package company-irony
+    :commands (company-irony)
+    :init
     (progn
-      (require 'company-rtags)
-      (add-to-list 'company-backends 'company-rtags)
-      (setq company-rtags-begin-after-member-access t)
-      (setq rtags-completions-enabled t)
-      )))
-
-(defun bl-c-c++/init-cmake-ide ()
-  (use-package cmake-ide
-    :init (with-eval-after-load 'rtags
-            (cmake-ide-setup))))
-
-;; Taken from https://github.com/syl20bnr/spacemacs/pull/2834/files
-(defun bl-c-c++/post-init-rtags ()
-  (setq company-rtags-begin-after-member-access nil)
-  (setq rtags-completions-enabled t)
-
-  (defun use-rtags (&optional useFileManager)
-    (and (rtags-executable-find "rc")
-         (cond ((not (gtags-get-rootpath)) t)
-               ((and (not (eq major-mode 'c++-mode))
-                     (not (eq major-mode 'c-mode))) (rtags-has-filemanager))
-               (useFileManager (rtags-has-filemanager))
-               (t (rtags-is-indexed)))))
-
-  (defun tags-find-symbol-at-point (&optional prefix)
-    (interactive "P")
-    (if (and (not (rtags-find-symbol-at-point prefix)) rtags-last-request-not-indexed)
-        (helm-gtags-find-tag)))
-
-  (defun tags-find-references-at-point (&optional prefix)
-    (interactive "P")
-    (if (and (not (rtags-find-references-at-point prefix)) rtags-last-request-not-indexed)
-        (helm-gtags-find-rtag)))
-
-  (defun tags-find-symbol ()
-    (interactive)
-    (call-interactively (if (use-rtags) 'rtags-find-symbol 'helm-gtags-find-symbol)))
-
-  (defun tags-find-references ()
-    (interactive)
-    (call-interactively (if (use-rtags) 'rtags-find-references 'helm-gtags-find-rtag)))
-
-  (defun tags-find-file ()
-    (interactive)
-    (call-interactively (if (use-rtags t) 'rtags-find-file 'helm-gtags-find-files)))
-
-  (defun tags-imenu ()
-    (interactive)
-    (call-interactively (if (use-rtags t) 'rtags-imenu 'idomenu)))
-
-  (dolist (mode '(c-mode c++-mode))
-    (evil-leader/set-key-for-mode mode
-      "m g ." 'rtags-find-symbol-at-point
-      "m g ," 'rtags-find-references-at-point
-      "m g v" 'rtags-find-virtuals-at-point
-      "m g V" 'rtags-print-enum-value-at-point
-      "m g /" 'rtags-find-all-references-at-point
-      "m g Y" 'rtags-cycle-overlays-on-screen
-      "m g >" 'rtags-find-symbol
-      "m g <" 'rtags-find-references
-      "m g [" 'rtags-location-stack-back
-      "m g ]" 'rtags-location-stack-forward
-      "m g D" 'rtags-diagnostics
-      "m g G" 'rtags-guess-function-at-point
-      "m g p" 'rtags-set-current-project
-      "m g P" 'rtags-print-dependencies
-      "m g e" 'rtags-reparse-file
-      "m g E" 'rtags-preprocess-file
-      "m g R" 'rtags-rename-symbol
-      "m g M" 'rtags-symbol-info
-      "m g S" 'rtags-display-summary
-      "m g O" 'rtags-goto-offset
-      "m g ;" 'rtags-find-file
-      "m g F" 'rtags-fixit
-      "m g L" 'rtags-copy-and-print-current-location
-      "m g X" 'rtags-fix-fixit-at-point
-      "m g B" 'rtags-show-rtags-buffer
-      "m g I" 'rtags-imenu
-      "m g T" 'rtags-taglist
-      "m g h" 'rtags-print-class-hierarchy
-      "m g a" 'rtags-print-source-arguments))
-
-  (rtags-enable-standard-keybindings)
-  (define-key c-mode-base-map (kbd "M-.") (function tags-find-symbol-at-point))
-  (define-key c-mode-base-map (kbd "M-,") (function tags-find-references-at-point))
-  (define-key c-mode-base-map (kbd "M-;") (function tags-find-file))
-  (define-key c-mode-base-map (kbd "C-.") (function tags-find-symbol))
-  (define-key c-mode-base-map (kbd "C-,") (function tags-find-references))
-  (define-key c-mode-base-map (kbd "C-<") (function rtags-find-virtuals-at-point))
-  (define-key c-mode-base-map (kbd "M-i") (function tags-imenu))
-
-  (define-key global-map (kbd "M-.") (function tags-find-symbol-at-point))
-  (define-key global-map (kbd "M-,") (function tags-find-references-at-point))
-  (define-key global-map (kbd "M-;") (function tags-find-file))
-  (define-key global-map (kbd "C-.") (function tags-find-symbol))
-  (define-key global-map (kbd "C-,") (function tags-find-references))
-  (define-key global-map (kbd "C-<") (function rtags-find-virtuals-at-point))
-  (define-key global-map (kbd "M-i") (function tags-imenu)))
-
-;;; packages.el ends here
+      (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+      (push 'company-irony company-backends-c-mode-common))))
