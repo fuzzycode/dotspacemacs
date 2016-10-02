acf46c37329b846e73b873951cd10e8a0af3975a
Björn Larsson <b.e.larsson@gmail.com>
2016-10-02 20:44:32 +0200

    Set fillcolumn to 120

--- layers/bl-c-c++/local/ff-c-style/ff-c-style.el
+++ layers/bl-c-c++/local/ff-c-style/ff-c-style.el
@@ -1,6 +1,7 @@
 ;;;###autoload
 (defconst ff-c-style
-  '((indent-tabs-mode . t)
+  '((fill-column . 120)
+    (indent-tabs-mode . t)
     (c-indent-level . 4)
     (tab-width . 4)
     (c-basic-offset . 4)
