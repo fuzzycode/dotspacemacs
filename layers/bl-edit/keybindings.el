71bf5ff686575613bbc79cf5e8a774d5934a8d3d
Björn Larsson <b.e.larsson@gmail.com>
2017-01-27 14:57:44 +0100

    Added key bindings to edit layer
    - Added Emacs navigation keys to evil-normal and evil-visual state
    - Added a key binding for sorting words

---
+++ layers/bl-edit/keybindings.el
@@ -0,0 +1,15 @@
+;; Evil-mode or no Evil mode, I NEED some of the emacs navigation keys
+(define-key evil-normal-state-map "\C-e" 'evil-end-of-line)
+(define-key evil-visual-state-map "\C-e" 'evil-end-of-line)
+(define-key evil-motion-state-map "\C-e" 'evil-end-of-line)
+(define-key evil-normal-state-map "\C-f" 'evil-forward-char)
+(define-key evil-normal-state-map "\C-b" 'evil-backward-char)
+(define-key evil-visual-state-map "\C-b" 'evil-backward-char)
+(define-key evil-normal-state-map "\C-d" 'evil-delete-char)
+(define-key evil-visual-state-map "\C-d" 'evil-delete-char)
+(define-key evil-normal-state-map "\C-n" 'evil-next-line)
+(define-key evil-visual-state-map "\C-n" 'evil-next-line)
+(define-key evil-normal-state-map "\C-p" 'evil-previous-line)
+(define-key evil-visual-state-map "\C-p" 'evil-previous-line)
+
+(spacemacs/set-leader-keys "ws" 'sort-words)
