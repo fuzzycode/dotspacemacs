b9adc9cafaeea7f14bc5970a6b289e3fac190c71
Björn Larsson <b.e.larsson@gmail.com>
2017-01-29 15:21:04 +0100

    Configure git commit message settings
    - Made the heading 50 wide and the message 72 wide.
    - Enable auto-fill and flyspell

--- layers/bl-git/config.el
+++ layers/bl-git/config.el
@@ -1,1 +1,8 @@
 (add-hook 'git-commit-mode-hook 'evil-insert-state)
+
+(eval-after-load 'magit (lambda ()
+                          (setq git-commit-summary-max-length 50)
+                          (setq git-commit-fill-column 72)
+
+                          (setq git-commit-turn-on-flyspell t)
+                          (setq git-commit-turn-on-auto-fill t)))
