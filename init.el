acdfef4a70e08745a5af13acdcd5060949812950
Björn Larsson <b.e.larsson@gmail.com>
2017-02-10 21:07:37 +0100

    Make dracula the default theme

--- init.el
+++ init.el
@@ -144,7 +144,8 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
+   dotspacemacs-themes '(dracula
+                         spacemacs-dark
                          spacemacs-light
                          solarized-light
                          solarized-dark
