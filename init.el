1e726c5d2b77772c47214289e627cf764fa83b9a
Björn Larsson <b.e.larsson@gmail.com>
2017-04-09 21:35:30 +0200

    Added auto-mode-alist settings

--- init.el
+++ init.el
@@ -406,4 +406,8 @@ you should place you code here."
 
   (when (file-exists-p "~/.snippets/")
     (add-to-list 'auto-completion-private-snippets-directory  "~/.snippets/"))
-  )
+
+  ;;; Setup the auto mode list for files without extensions
+  (add-to-list 'auto-mode-alist '(".clang-format\\" . yaml-mode))
+  (add-to-list 'auto-mode-alist '(".clang-tidy\\" . yaml-mode))
+)
