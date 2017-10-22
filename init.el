5c8f99358dcb5a79b034471e1b39cecb18f41306
Björn Larsson <b.e.larsson@gmail.com>
2017-10-22 21:58:52 +0200

    Disable rtags
    Testing to get ycmd working for now

--- init.el
+++ init.el
@@ -99,7 +99,7 @@ values."
      bl-edit
      ;;bl-debug
      (bl-c-c++ :variables
-               bl-c-c++-enable-rtags t)
+               bl-c-c++-enable-rtags nil)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
