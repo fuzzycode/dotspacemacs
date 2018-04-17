a8bec8b73d3634f2c790e8f60787717835d28e90
Björn Larsson <b.e.larsson@gmail.com>
2018-04-17 21:33:43 +0200

    Improve magit dracula faces

--- layers/bl-edit/config.el
+++ layers/bl-edit/config.el
@@ -71,6 +71,7 @@
 
     ;; Magit
     (magit-diff-lines-boundary :foreground "#f1fa8c" :background "#f1fa8c" :inherit magit-diff-lines-heading)
+    (magit-section-secondary-heading :inherit magit-section-heading)
 
     ;; Misc
     (sp-show-pair-match-face :underline t)
