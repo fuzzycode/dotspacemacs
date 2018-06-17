db266ff0fd365224d45b0166c9f9b3b16795eb64
Björn Larsson <b.e.larsson@gmail.com>
2018-06-17 15:30:54 +0200

    Defer semantic mode

--- init.el
+++ init.el
@@ -533,6 +533,7 @@ you should place you code here."
   ;; Make sure that semantic does not go crazy on elisp comments
   ;; https://github.com/syl20bnr/spacemacs/pull/7736#issuecomment-313320906
   (use-package semantic
+    :defer t
     :config
     (setq-mode-local emacs-lisp-mode
                      semanticdb-find-default-throttle
