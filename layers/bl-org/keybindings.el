7fbe3722932ccd9150500cdc4ce98401e4494b8f
Björn Larsson <b.e.larsson@gmail.com>
2019-03-25 19:52:36 +0100

    Add file comments

--- layers/bl-org/keybindings.el
+++ layers/bl-org/keybindings.el
@@ -1,3 +1,20 @@
+;;; keybindings.el --- bl-org layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2019 Sylvain Benner & Contributors
+;;
+;; Author: Björn Larsson <develop@bjornlarsson.net>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
 (global-set-key (kbd "C-c i") 'bl-org/open-inbox)
 
 (spacemacs/set-leader-keys "ooi" 'bl-org/open-inbox)
+
+;;; keybindings.el ends here
