dd857d5d7d0b6ca3e2903b9e7d6d159f8a74433d
Björn Larsson <b.e.larsson@gmail.com>
2019-04-13 21:00:07 +0200

    Fix loading of alert package

--- layers/bl-edit/funcs.el
+++ layers/bl-edit/funcs.el
@@ -17,7 +17,7 @@
 ;;   SPC h SPC layers RET
 ;;
 ;;; Code:
-(require 'alert)
+
 
 
 (defun bl-edit/goto-first-empty-line (&optional from-top)
@@ -47,6 +47,7 @@
 
 (defun bl-edit/maybe-notify-compile-finish (buffer string)
   "Show an alert when compilation finished, like XCode does"
+  (require 'alert)
   (when bl-edit-notify-compile-finished
     (if (string-match "^finished" string)
         (alert "Compilation finished OK!" :title "Compilation Successful" :category 'compile :id 'compile-ok)
