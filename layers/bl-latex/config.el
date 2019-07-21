d0a0b24d46c446a6da8aa7efd159270097e8fed4
Björn Larsson <b.e.larsson@gmail.com>
2019-07-21 21:58:11 +0200

    Add perl-mode for latexmkrc files

---
+++ layers/bl-latex/config.el
@@ -0,0 +1,21 @@
+;;; config.el --- bl-latex layer configuration file for Spacemacs.
+;;
+;; Copyright (c) 2012-2019 Sylvain Benner & Contributors
+;;
+;; Author: Björn Larsson <develop@bjornlarsson.net>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;; Code:
+
+(add-to-list 'auto-mode-alist '("\\.latexmkrc\\'" . perl-mode))
