feeafc7bc7e6fde3548379991de11128a2d4c1ba
Björn Larsson <b.e.larsson@gmail.com>
2019-08-22 18:04:00 +0200

    Remove Hound
    It did not work as expected.

--- layers/bl-edit/packages.el
+++ layers/bl-edit/packages.el
@@ -44,7 +44,6 @@
     helm-swoop
     helpful
     highlight-doxygen
-    hound
     keyfreq
     lsp-ui
     lsp-origami
@@ -89,13 +88,6 @@ Each entry is either:
 
       - A list beginning with the symbol `recipe' is a melpa
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
-
-(defun bl-edit/init-hound ()
-  "Emacs interface to git-hound, needs to be configured to point to the right back-end
-https://github.com/ryoung786/hound.el"
-  (use-package hound
-    :defer t
-    :commands hound))
 
 (defun bl-edit/pre-init-doom-modeline ()
   (spacemacs|use-package-add-hook doom-modeline
