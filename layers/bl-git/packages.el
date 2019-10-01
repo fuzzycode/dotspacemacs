604425f617ab5bc05df665ec9d2f854af180f2de
Björn Larsson <b.e.larsson@gmail.com>
2019-10-01 19:02:33 +0200

    Remove treemacs-magit
    It is now owned by spacemacs

--- layers/bl-git/packages.el
+++ layers/bl-git/packages.el
@@ -32,7 +32,6 @@
 (defconst bl-git-packages
   '(magit-imerge
     magit-todos
-    (treemacs-magit :toggle (configuration-layer/package-used-p 'treemacs))
     rigid-tabs)
   "The list of Lisp packages required by the bl-git layer.
 
@@ -60,13 +59,6 @@ Each entry is either:
 
       - A list beginning with the symbol `recipe' is a melpa
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
-
-(defun bl-git/init-treemacs-magit ()
-  (use-package treemacs-magit
-    :after treemacs
-    :defer t
-    :init (require 'treemacs-magit)))
-
 
 (defun bl-git/init-rigid-tabs ()
   (use-package rigid-tabs
